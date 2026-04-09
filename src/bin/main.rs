805953ba9ad6761379329ffcc2e0705fabb88055
Matt Oestreich <matthewpoestreich@gmail.com>
2026-04-09 14:22:41 -0500

    update main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,31 +1,40 @@
 use calcinum::parse_expression;
 use std::{env, process};
 
+const EXPECTED_ARGS_LEN: usize = 1;
+
 fn main() {
-    let mut args = env::args().skip(1).peekable();
+    let args: Vec<_> = env::args().skip(1).collect();
 
-    if let Some(v) = args.peek()
-        && (v == "--version" || v == "-v")
-    {
-        let version = env!("CARGO_PKG_VERSION");
-        println!("{version}");
-        process::exit(0);
-    }
+    validate_argc(args.len());
+    let arg = args.first().expect("verified argc > 0");
 
-    match args.next() {
-        None => {
-            eprintln!("Missing argument! Please provide an expression as a string, e.g., '2 + 2'");
-            process::exit(1);
+    match arg.as_str() {
+        "--version" | "-v" => {
+            println!("{}", env!("CARGO_PKG_VERSION"));
+            process::exit(0);
         }
-        Some(ref expression) => match parse_expression(expression) {
+        s => match parse_expression(s) {
             Ok(r) => {
                 println!("{r}");
                 process::exit(0);
             }
             Err(e) => {
-                eprintln!("ERROR parsing expression\n\n{expression}\n\n{e}");
+                eprintln!("ERROR parsing expression\n\n{s}\n\n{e}");
                 process::exit(1);
             }
         },
+    }
+}
+
+/// Checks that number of received args matches number of expected args.
+fn validate_argc(argc: usize) {
+    if argc == 0 {
+        eprintln!("Missing argument! Please provide an expression as a string, e.g., '2 + 2'");
+        process::exit(1);
+    }
+    if argc != EXPECTED_ARGS_LEN {
+        eprintln!("ERROR expected {EXPECTED_ARGS_LEN} argument(s), got {argc}");
+        process::exit(1);
     }
 }
